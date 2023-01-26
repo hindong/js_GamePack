d7e54f90e7b00f4c6bb46c2da9ca818aad284e0d
bu <86216382+hindong@users.noreply.github.com>
2023-01-27 03:22:15 +0900

    캔버스 잔상 제거

--- PingPong/player.js
+++ PingPong/player.js
@@ -1,5 +1,7 @@
 
 const speed = 4;
+const keyUp = "KeyW";
+const keyDown = "KeyS";
 
 export function playerDraw(player, ctx){
     ctx.fillStyle = "rgb(200,0,0)";
@@ -7,14 +9,21 @@ export function playerDraw(player, ctx){
 }
 
 // 키를 누르면 플레이어 움직임 (방향키)
-// key: ArrowDown , ArrowUp
+// key -  w : up , s : down
 export function movePlayer(player, ctx, e){
-    if(e == "ArrowDown"){
+    if(e == keyDown){
+        clear(player, ctx);
         player.yPos += speed;
         playerDraw(player, ctx);
     }
-    if(e == "ArrowUp"){
+    if(e == keyUp){
+        clear(player, ctx);
         player.yPos -= speed;
         playerDraw(player, ctx);
     }   
+}
+
+// canvas 잔상을 지워주는 함수
+export function clear(object, ctx){
+    ctx.clearRect(object.xPos, object.yPos, 10, 50);
 }
