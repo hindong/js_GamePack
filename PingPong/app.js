58c83bd888983bb742c077f7eba469b6c11f0ab0
bu <86216382+hindong@users.noreply.github.com>
2023-03-07 18:34:28 +0900

    코드 리펙토링

--- PingPong/app.js
+++ PingPong/app.js
@@ -1,35 +1,7 @@
-import { movePlayer, drawPlayer } from "./player.js";
-import { drawBall, moveBall, animate } from "./ball.js";
+import { start } from "./game.js";
 
 export const canvas = document.querySelector("#main");
 export const ctx = canvas.getContext("2d");
 
-const player = {
-    xPos: 530,
-    yPos: 450,
-    move: true,
-};
 
-const computer = {
-    xPos: 10,
-    yPos: 10,
-    move: true,
-}
-
-// computer
-drawPlayer(computer, ctx);
-
-// user
-drawPlayer(player, ctx);
-drawBall();
-
-
-animate();
-
-setTimeout(() => {
-    document.addEventListener('keypress', (e) =>{
-        if(e.code){
-            movePlayer(player, ctx, e.code);
-        }
-    })    
-}, 50);
+start();
