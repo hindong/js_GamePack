fde6dbf4b0cb53c10a39f9b3a981a0ef568cb149
bu <86216382+hindong@users.noreply.github.com>
2023-03-15 07:26:30 +0900

    함수명 수정

--- PingPong/app.js
+++ PingPong/app.js
@@ -1,7 +1,7 @@
-import { start } from "./game.js";
+import { initGame } from "./game.js";
 
 export const canvas = document.querySelector("#main");
 export const ctx = canvas.getContext("2d");
 
 
-start();
+initGame();
